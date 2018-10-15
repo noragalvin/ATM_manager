0eb21b5dc8ab1bfb089f436b8265e89fbc79ac1e
noragalvin <minhnora98@gmail.com>
2018-10-15 14:29:59 +0700

    ADD validate, change language and term forms

---
+++ ATM_Manager/SV1/Validation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SV1
+{
+    public partial class Validation : Form
+    {
+        public Validation()
+        {
+            InitializeComponent();
+        }
+
+        private void pnDutThe_Click(object sender, EventArgs e)
+        {
+            ChooseLanguage cl = new ChooseLanguage();
+            this.Hide();
+            cl.ShowDialog();
+            
+        }
+
+
+
+
+
+
+
+
+    }
+}
